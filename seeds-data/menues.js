678efbef90158005b2638df8905867b88b355ff4
Trishane <trishaneww@gmail.com>
2024-04-26 23:50:00 -0400

    created knex migration

---
+++ seeds-data/menues.js
@@ -0,0 +1,12 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+module.exports = [
+    {
+        id: 1,
+        store_id: 1,
+        title: "All Day Menu"
+    }
+]
